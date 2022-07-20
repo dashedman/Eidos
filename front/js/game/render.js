34b141428b89e27b3cdad9ecdc7c1fededc67aca
dashed_man <assarbsmail@gmail.com>
2022-07-20 13:16:41 +0300

    fix rect for physbox

--- front/js/game/render.js
+++ front/js/game/render.js
@@ -10,6 +10,7 @@ export default class Renderer extends EngineRenderer {
             for(let [targ, rect] of this.debugShapes.entries()){
                 rect.rx = targ.x
                 rect.ry = targ.y
+                rect.rz = 1
                 rect.rw = targ.w
                 rect.rh = targ.h
             }
